4f57923914c353791de40fd31094a63e613f330c
Dallas Blackwell <mdvayne923@gmail.com>
2020-05-20 18:37:21 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
 def starts_with_a_vowel?(word)
-
+  word.match/[aeiou]/
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.grep /^\w{5}\s/
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match/[A-Z]/
+  #\
+  #End with puncuation --> \W
 end
 
 def valid_phone_number?(phone)
-
+  phone.scan(/(\d{3})-(\d{3})-(\d{4})/)
 end
